94f218843f5ad22e12a987c671c3972634e27760
Luka Čičak <cicak.luka@gmail.com>
2026-05-28 14:00:03 +0200

    Defined routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,45 @@
-import { Routes } from '@angular/router';
-import { Home } from './features/home/home';
-import { AppRoutes } from './shared/utils/routes';
-import { QuizList } from './features/quiz-list/quiz-list';
-import { QuizDetails } from './features/quiz-details/quiz-details';
-import { Leaderboard } from './features/leaderboard/leaderboard';
-import { CookiePolicy } from './features/cookie-policy/cookie-policy';
-import { PrivacyPolicy } from './features/privacy-policy/privacy-policy';
+import { Routes } from "@angular/router";
+import { Home } from "./features/home/home";
+import { AppRoutes } from "./shared/utils/routes";
+import { QuizList } from "./features/quiz-list/quiz-list";
+import { QuizDetails } from "./features/quiz-details/quiz-details";
+import { Leaderboard } from "./features/leaderboard/leaderboard";
+import { CookiePolicy } from "./features/cookie-policy/cookie-policy";
+import { PrivacyPolicy } from "./features/privacy-policy/privacy-policy";
+import { Constants } from "./shared/utils/constants";
 
 export const routes: Routes = [
-  {
-    path: AppRoutes.home,
-    component: Home,
-  },
-  {
-    path: AppRoutes.quizList,
-    component: QuizList,
-  },
-  {
-    path: AppRoutes.quizDetails,
-    component: QuizDetails,
-  },
-  {
-    path: AppRoutes.leaderboard,
-    component: Leaderboard,
-  },
-  {
-    path: AppRoutes.cookiePolicy,
-    component: CookiePolicy,
-  },
-  {
-    path: AppRoutes.privacyPolicy,
-    component: PrivacyPolicy,
-  },
+	{
+		path: AppRoutes.home.path,
+		component: Home,
+		title: Constants.appName,
+	},
+	{
+		path: AppRoutes.quizList.path,
+		component: QuizList,
+		title: `${Constants.appName} - ${AppRoutes.quizList.title}`,
+	},
+	{
+		path: AppRoutes.quizDetails.path,
+		component: QuizDetails,
+	},
+	{
+		path: AppRoutes.leaderboard.path,
+		component: Leaderboard,
+		title: `${Constants.appName} - ${AppRoutes.leaderboard.title}`,
+	},
+	{
+		path: AppRoutes.cookiePolicy.path,
+		component: CookiePolicy,
+		title: `${Constants.appName} - ${AppRoutes.cookiePolicy.title}`,
+	},
+	{
+		path: AppRoutes.privacyPolicy.path,
+		component: PrivacyPolicy,
+		title: `${Constants.appName} - ${AppRoutes.privacyPolicy.title}`,
+	},
+	{
+		path: "**",
+		redirectTo: AppRoutes.home.path,
+	},
 ];
--- src/app/shared/utils/routes.ts
+++ src/app/shared/utils/routes.ts
@@ -1,8 +1,13 @@
-export const AppRoutes = {
-	home: "",
-	quizList: "quiz-list",
-	quizDetails: "quiz-details",
-	leaderboard: "leaderboard",
-	privacyPolicy: "privacy-policy",
-	cookiePolicy: "cookie-policy",
+export type tRoute = {
+	path: string;
+	title: string;
 };
+
+export const AppRoutes = {
+	home: { path: "", title: "Početna" },
+	quizList: { path: "kvizovi", title: "Kvizovi" },
+	quizDetails: { path: "kviz/:id", title: "Kviz" },
+	leaderboard: { path: "leaderboard", title: "Leaderboard" },
+	privacyPolicy: { path: "politika-privatnosti", title: "Politika Privatnosti" },
+	cookiePolicy: { path: "politika-kolacica", title: "Politika Kolačića" },
+} as const satisfies Record<string, tRoute>;
