6353068ab3a070bdf17f0bed1cd78d15ab7e3dc8
Luka Čičak <cicak.luka@gmail.com>
2026-05-28 13:59:49 +0200

    Added footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AppName } from "../app-name/app-name";
+import { Constants } from "../utils/constants";
+import { AppRoutes, tRoute } from "../utils/routes";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+	selector: "app-footer",
+	imports: [RouterLink, RouterLinkActive, AppName],
+	templateUrl: "./footer.component.html",
+	styleUrl: "./footer.component.scss",
 })
 export class FooterComponent {
+	protected appName = Constants.appName;
 
+	protected footerRoutes: Array<tRoute> = [
+		AppRoutes.home,
+		AppRoutes.quizList,
+		AppRoutes.leaderboard,
+		AppRoutes.privacyPolicy,
+		AppRoutes.cookiePolicy,
+	];
 }
